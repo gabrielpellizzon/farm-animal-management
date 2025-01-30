011c97cdce3c5783cdab64e9b60ee6395db6a2d1
gabrielpellizzon <gabrielpellizzon@gmail.com>
2025-01-30 20:28:30 -0300

    feat - layout screen enhancements

--- src/app/modules/farm/farm.module.ts
+++ src/app/modules/farm/farm.module.ts
@@ -6,9 +6,10 @@ import { FarmListComponent } from './components/farm-list/farm-list.component';
 import { FarmFormComponent } from './components/farm-form/farm-form.component';
 import { FarmViewComponent } from './pages/farm-view/farm-view.component';
 import { SharedModule } from '../../shared/shared.module';
+import { TemplateModule } from '../../template/template.module';
 
 @NgModule({
   declarations: [FarmListComponent, FarmFormComponent, FarmViewComponent],
-  imports: [CommonModule, FarmRoutingModule, SharedModule],
+  imports: [CommonModule, FarmRoutingModule, SharedModule, TemplateModule],
 })
 export class FarmModule {}
